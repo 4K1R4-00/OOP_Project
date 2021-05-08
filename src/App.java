6d7547917f9e3188d703a5f45c9424531c76e963
AizOfMulia <aizofmulia@gmail.com>
2021-05-08 14:37:33 +0800

    Added App init

--- src/App.java
+++ src/App.java
@@ -2,23 +2,41 @@
  *  The App class contains all the application logic needed to operate the program
  *
  */
+import java.util.Scanner;
+
 class App
 {
     //  either TUI or GUI type setting
-    private int type;
+    private int[] type = {1, 2};
 
     //  Item product and services array
     private Item[] items    =   new Item[10];
 
-    //  Default constructor 
+    //  Default constructor
     App() 
     {
         System.out.println("The app has been instantiated");
+
+        Scanner input   =   new Scanner(System.in);
+        int appType     =   input.nextInt();
+
+        System.out.println("Select application runtime mode  ");
+        System.out.println("=================================");
+        System.out.println("1. TUI Mode");
+        System.out.println("2. GUI Mode");
+        System.out.print("Enter Selection: ");
+
+        if (appType == type[0])
+            startTui();
+
+        if (appType == type[1])
+            startGui();
     }
 
-    //  Start the app
-    public void start()
-    {
+    //  Start TUI
+    public void startTui() {}
 
-    }
+    //  Start GUI
+    public void startGui() {}
+
 }
