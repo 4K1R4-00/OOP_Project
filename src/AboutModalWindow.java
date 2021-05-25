d59939fa1df32378d34bdf29937e061245c7445a
phineasandferb97radzi <phineasandferb97radzi@gmail.com>
2021-05-25 14:01:55 +0800

    Ammend about dialog

--- src/AboutModalWindow.java
+++ src/AboutModalWindow.java
@@ -1,9 +1,11 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class AboutModalWindow {
     private JFrame parentFrame;
-    private String developer = "";
-
+    private JOptionPane dialog;
+    private JPanel aboutPanel   = new JPanel(new GridLayout(0,1,0,5));
+    private String developer    = "";
 
 
     AboutModalWindow(JFrame parentFrame){
@@ -11,16 +13,21 @@ public class AboutModalWindow {
     }
 
     public void displayInfo(){
-        setDeveloper("Akmal");
-        setDeveloper("Faiz");
-        setDeveloper("Danish");
-        setDeveloper("Arifa");
+       // Message Content
+        message("==== Developed By ====");
+        message("Akmal");
+        message("Faiz");
+        message("Danish");
+        message("Arifa");
 
-        JOptionPane.showMessageDialog(parentFrame,"Developer of P.O.S Salon: \n" + this.developer, "About (Ver 1.0)", JOptionPane.PLAIN_MESSAGE);
+
+
+        dialog.showMessageDialog(parentFrame, aboutPanel, "About POS System (Ver 1.0)", JOptionPane.PLAIN_MESSAGE);
+
     }
 
-    public void setDeveloper(String name){
-        this.developer  +=  "" + name + "\n";
+    public void message(String content){
+        aboutPanel.add(new JLabel(content, SwingConstants.CENTER));
 
     }
 
